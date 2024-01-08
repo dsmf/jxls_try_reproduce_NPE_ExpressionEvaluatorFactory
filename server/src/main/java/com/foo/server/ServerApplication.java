8ed1c1c04c6e450edabd252e111911bc9992ce4c
Matthias Fischer <matthias.fischer@doubleslash.de>
2024-01-08 18:14:48 +0100

    remove superfluous

--- server/src/main/java/com/foo/server/ServerApplication.java
+++ server/src/main/java/com/foo/server/ServerApplication.java
@@ -11,14 +11,4 @@ public class ServerApplication {
         SpringApplication.run(ServerApplication.class, args);
     }
 
-//    @Bean
-//    public Executor taskExecutor() {
-//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-//        executor.setCorePoolSize(2);
-//        executor.setMaxPoolSize(2);
-//        executor.setQueueCapacity(500);
-//        executor.setThreadNamePrefix("ASYNC-EXECUTOR-");
-//        executor.initialize();
-//        return executor;
-//    }
 }
